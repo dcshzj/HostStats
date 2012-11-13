55c7ba2dc4953ed09c86a5bc7938f6591c08a201
Hydriz <admin@alphacorp.tk>
2012-11-14 00:20:55 +0800

    Sync "Localisation updates from translatewiki.net." from Gerrit.
    Added localisation support for:
    1) German (de)
    2) Spanish (es)
    3) French (fr)
    4) Galician (gl)
    5) Hebrew (he)
    6) Hungarian (hu)
    7) Italian (it)
    8) Japanese (ja)
    9) Luxembourgish (lb)
    10) Macedonian (mk)

--- HostStats.i18n.php
+++ HostStats.i18n.php
@@ -20,9 +20,101 @@ $messages['en'] = array(
 
 /** Message documentation (Message documentation)
  * @author Hydriz
+ * @author Shirayuki
  */
 $messages['qqq'] = array(
+	'hoststats' => '{{notranslate}}',
 	'hoststats-title' => 'The title of the special page. "Host" refers to the server the wiki is on.',
-	'hoststats-desc' => '{{desc}}',
+	'hoststats-desc' => '{{desc|name=Host Stats|url=http://www.mediawiki.org/wiki/Extension:HostStats}}',
 	'hoststats-intro' => 'The introduction to the special page. "Host" refers to the server the wiki is on.',
+);
+
+/** German (Deutsch)
+ * @author Metalhead64
+ */
+$messages['de'] = array(
+	'hoststats-title' => 'Hoststatistiken',
+	'hoststats-desc' => 'Ergänzt ein Werkzeug zur Ausgabe von Statistiken des Hosts dieses Wikis',
+	'hoststats-intro' => 'Diese Seite zeigt einige Statistiken des Hosts dieses Wikis an.',
+);
+
+/** Spanish (español)
+ * @author Armando-Martin
+ */
+$messages['es'] = array(
+	'hoststats-title' => 'Estadísticas del servidor',
+	'hoststats-desc' => 'Una herramienta muestra las estadísticas del servidor en que encuentra la wiki.',
+	'hoststats-intro' => 'Esta página muestra algunas estadísticas del servidor en que se encuentra el wiki.',
+);
+
+/** French (français)
+ * @author Gomoko
+ */
+$messages['fr'] = array(
+	'hoststats-title' => "Statistiques de l'hôte",
+	'hoststats-desc' => "Un outil pour sortir les statistiques de l'hôte sur lequel se trouve le wiki.",
+	'hoststats-intro' => "Cette page affiche des statistiques de l'hôte sur lequel se trouve le wiki.",
+);
+
+/** Galician (galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'hoststats-title' => 'Estatísticas do servidor',
+	'hoststats-desc' => 'Unha ferramenta que mostra as estatísticas do servidor no que se atopa este wiki.',
+	'hoststats-intro' => 'Esta páxina mostra algunhas estatísticas do servidor no que se atopa este wiki.',
+);
+
+/** Hebrew (עברית)
+ * @author Amire80
+ */
+$messages['he'] = array(
+	'hoststats-title' => 'סטטיסטיקות מארח',
+	'hoststats-desc' => 'כלי לפליטת סטטיסטיקות על המארח של הוויקי',
+	'hoststats-intro' => 'הדף הזה מציג סטטיסטיקות על הדף השוויקי נמצא בו.',
+);
+
+/** Hungarian (magyar)
+ * @author Tgr
+ */
+$messages['hu'] = array(
+	'hoststats-title' => 'Szerverstatisztika',
+	'hoststats-desc' => 'Statisztikákat jelenít meg arról a szerverről, amiről a wiki üzemel.',
+	'hoststats-intro' => 'Statisztikákat mutat a szerverről, amiről a wiki üzemel.',
+);
+
+/** Italian (italiano)
+ * @author Beta16
+ */
+$messages['it'] = array(
+	'hoststats-title' => "Statistiche relative all'host",
+	'hoststats-desc' => "Uno strumento per mostrare le statistiche riguardo all'host in cui si trova questo wiki.",
+	'hoststats-intro' => 'Questa pagina mostra alcune statistiche riguardo al server in cui viene ospitato questo wiki.',
+);
+
+/** Japanese (日本語)
+ * @author Shirayuki
+ */
+$messages['ja'] = array(
+	'hoststats-title' => 'ホストに関する統計',
+	'hoststats-desc' => 'ウィキが動作しているホストに関する統計を出力するツール',
+	'hoststats-intro' => 'このページでは、ウィキが動作しているホストに関する統計を表示します。',
+);
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'hoststats-title' => 'Host-Statistiken',
+	'hoststats-desc' => "En Tool fir d'Statistike vum Host ze wesien op deem d'Wiki drop steet.",
+	'hoststats-intro' => "Dës Säit weist e puer Statistike vum Host wou d'Wiki drop steet.",
+);
+
+/** Macedonian (македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'hoststats-title' => 'Статистики за домаќинот',
+	'hoststats-desc' => 'Алатка за испис на статистики за домаќинот (опслужувачот на викито).',
+	'hoststats-intro' => 'На страницава се пркажани извсни статистики за домаќинот на викито.',
 );
